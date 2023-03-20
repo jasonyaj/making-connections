bee825c639893e0abfb65999673aaa7a474d871d
jasonyaj <jason.yaj@gmail.com>
2023-03-20 17:38:02 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeUser(element) {
+  element.innerText = "Jason Y";
+}
+
+function removeFromList(element) {
+  document.getElementById(element.remove());
+}
+
+function plusConnections(element) {
+  element.innerText++;
+}
+
+function minusConnections(element) {
+  element.innerText--;
+}
